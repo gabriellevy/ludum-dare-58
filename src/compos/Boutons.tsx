55142644c2e8e07a9d1cf38910cab6b72f2bcc0f
gabriellevy <mdeharbe@hotmail.com>
2025-10-04 15:39:31 +0200

    consommer champis

--- src/compos/Boutons.tsx
+++ src/compos/Boutons.tsx
@@ -1,10 +1,23 @@
-import {useContext} from 'react';
+import {useCallback, useContext} from 'react';
 import {Avatar, Box, Button, Tooltip} from '@mui/material';
 import {PersoContexte, PersoContexteType} from "../contexte/ContexteType";
 import {Champignon, ChampignonEnum, champignons} from "../types/Champignon";
 
 export default function Boutons() {
     const { perso, setPerso } = useContext(PersoContexte) as PersoContexteType;
+
+    const activerChampi = useCallback((champi: Champignon, index:number) => {
+        let persoTmp = perso;
+        champi.effet(persoTmp);
+        persoTmp.champignons = [
+            ...perso.champignons.slice(0, index),
+            ...perso.champignons.slice(index+1, perso.champignons.length-1)
+        ];
+
+        setPerso({
+            ...persoTmp,
+        });
+    }, [perso, setPerso]);
 
     return (
         <Box
@@ -14,13 +27,14 @@ export default function Boutons() {
             }}
         >
             {
-                perso.champignons.map((champiEnum:ChampignonEnum) => {
+                perso.champignons.map((champiEnum:ChampignonEnum, index:number) => {
                     const champi: Champignon = champignons[champiEnum];
                     return (<Tooltip title={champi.nom}>
                             <Button
                                 variant="contained"
                                 color="primary"
                                 startIcon={<Avatar src={champi.imageSrc} />}
+                                onClick={() => activerChampi(champi, index)}
                             />
                         </Tooltip>
                     )}
